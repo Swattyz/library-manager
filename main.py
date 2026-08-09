afcba1be0b5c849b718a217151000e65aad1c014
$wat <ruanbiasuttidesouza@gmail.com>
2026-08-08 21:36:28 -0300

    Adicionado função de busca de livro + troca de nome do arquivo .CSV + adição de conteúdo no arquivo README.md + menores mudanças no código (08/08)

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ estrutura = ['titulo', 'autor', 'ano', 'isbn', 'disponivel']
 def cadastrar_livro(titulo,autor,ano):
     book_qty = int(0)
     
-    with open('sheet.csv', 'r') as file:
+    with open('livros.csv', 'r') as file:
         for line in file:
             book_qty += 1
             isbn = int(9786599876500) + (book_qty-1)
@@ -20,7 +20,7 @@ def cadastrar_livro(titulo,autor,ano):
     998765 --> código-exemplo que indica editoras pequenas 
     00 --> código que indica qual livro é (o primeiro livro é 00, o segundo registrado será 01, etc.)'''
 
-    with open('sheet.csv','a',newline='') as f:
+    with open('livros.csv','a',newline='') as f:
             produto = {'titulo':titulo, 'autor':autor, 'ano':ano, 'isbn':isbn, 'disponivel':"Disponivel"}
             writer = csv.DictWriter(f,fieldnames=estrutura)
             writer.writerow(produto)
@@ -31,7 +31,7 @@ def emprestimo_devolucao(isbn, mode):
     lines = []
     was_found = 0
 
-    with open("sheet.csv", "r", newline='') as file:
+    with open("livros.csv", "r", newline='') as file:
         reader = csv.reader(file)
         for items in reader:
             if items[3] == isbn:
@@ -41,33 +41,48 @@ def emprestimo_devolucao(isbn, mode):
                     if items[4] == "Disponivel":
                         items[4] = "Indisponivel"
                     else:
-                        return "\nEsse livro não está disponível!"
+                        return f"\nO livro '{items[1]}' não está disponível!"
                     
                 else:
                     if items[4] == "Indisponivel":
                         items[4] = "Disponivel"
                     else:
-                        return "\nEsse livro já está disponível!"
+                        return f"\nO livro '{items[1]}' já está disponível!"
                     
             lines.append(items)
 
-    with open("sheet.csv", "w", newline='') as file:
+    with open("livros.csv", "w", newline='') as file:
         writer = csv.writer(file)
         writer.writerows(lines)
 
-    if was_found == 1:
+    if was_found == 1 and mode == "Emprestimo":
         return "\nLivro emprestado com sucesso."
+    elif was_found == 1 and mode == "Devolucao":
+        return "\nLivro devolvido com sucesso."
     else:
         return "\nNenhum livro encontrado!"
 
 def listar_livros():
-    with open("sheet.csv", "r") as file:
+    with open("livros.csv", "r") as file:
         reader = csv.DictReader(file)
         counter = 1
 
         for line in reader:
-            print(f"\n{counter}. {line["titulo"]} por {line["autor"]} em {line["ano"]} \nISBN: {line["isbn"]} | Status: {line["disponivel"]}")
+            print(f"\n{counter}. '{line["titulo"]}' por {line["autor"]} em {line["ano"]} \nISBN: {line["isbn"]} | Status: {line["disponivel"]}")
             counter += 1
+
+def buscar_livro(value,mode):
+    with open("livros.csv", "r") as file:
+        counter = 1
+        reader = csv.reader(file)
+
+        for line in reader:
+            if str(line[mode]) == value:
+                print (f"\n{counter}. '{line[0]}' por {line[1]} em {line[2]} \nISBN: {line[3]} | Status: {line[4]}")
+                counter += 1
+
+        if counter == 1:
+            print("\nLivro não encontrado.")
 
 # CÓDIGO PRINCIPAL
 
@@ -104,6 +119,7 @@ while True:
                     print("Valor inválido!\n")
                     continue
             print(cadastrar_livro(titulo,autor,ano))
+            input("\n(Pressione 'Enter' para continuar)\n")
 
         case 2 | 3:
             while True:
@@ -113,9 +129,9 @@ while True:
                     mode = "Devolucao"
                 
                 try:
-                    isbn = int(input("\nInsira o ISBN do livro: "))
+                    isbn = int(input("\n- Insira o ISBN do livro: "))
                 except:
-                    print("Não é um ISBN com formato válido!\n")
+                    print(f"'{isbn}' não é um ISBN com formato válido!\n")
                     continue
 
                 if len(str(isbn)) != 13:
@@ -123,18 +139,51 @@ while True:
                     continue
                 break
             print(emprestimo_devolucao(str(isbn), mode))
+            input("\n(Pressione 'Enter' para continuar)\n")
 
         case 4:
             listar_livros()
+            input("\n(Pressione 'Enter' para continuar)\n")
 
         case 5:
-            pass
+            while True:
+                try:
+                    mode = int(input("\nEscolha o tipo de característica do livro queira utilizar para realizar a busca: " \
+                    "\n[0] ---> Buscar por título." \
+                    "\n[1] ---> Buscar por autor." \
+                    "\n[2] ---> Buscar por ano de lançamento." \
+                    "\n[3] ---> Buscar por ISBN." \
+                    "\n[4] ---> Buscar por disponível." \
+                    "\n[5] ---> Buscar por indísponível." \
+                    "\n--> "))
+                except:
+                    print(f"O valor dado não é válido!\n")
+                    continue
+
+                if mode not in [0,1,2,3,4,5]:
+                    print(f"'{mode}' inválido! Tente novamente.\n")
+                    continue
+
+                elif mode in [4,5]:
+                    if mode == 4:
+                        value = "Disponivel"
+
+                    elif mode == 5:
+                        mode = 4
+                        value = "Indisponivel"
+
+                else:
+                    value = input("\nDeclare o valor que queira buscar: ")
+
+                buscar_livro(value,mode)
+                input("\n(Pressione 'Enter' para continuar)\n")
+                break
 
         case 6:
             pass
 
         case 7:
-            print("\nSistema encerrado.")
+            print("\n- SISTEMA ENCERRADO -")
             break
 
         case _:
